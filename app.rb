a505d0186fc3b1724e7736d161c361a0757494e7
Mike Gehard <mike.gehard@gmail.com>
2014-02-10 14:26:30 -0700

    Display a list of items on the /items page.

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
+set :items, {1 => "Channa Masala", 2 => "Chicken Tikka Masala", 3 => "Saag Paneer"}
+
 get '/' do
   erb :index
 end
 
 get '/items' do
-  "This is the items page"
+  erb :items, locals: {items: settings.items}
 end
