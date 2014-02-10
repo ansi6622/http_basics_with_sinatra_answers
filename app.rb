00248715aa3bac5e2669828829acf07710a4a79a
Mike Gehard <mike.gehard@gmail.com>
2014-02-10 14:45:05 -0700

    Filter the items based on a query param.

--- app.rb
+++ app.rb
@@ -7,7 +7,18 @@ get '/' do
 end
 
 get '/items' do
-  puts "*" * 30
-  puts params['filter']
-  erb :items, locals: {items: settings.items}
+  filter = params['filter']
+
+  if filter.nil?
+    matched_items = settings.items
+  else
+    matched_items = {}
+    settings.items.each do |id, name|
+      if name.downcase.include?(filter.downcase)
+        matched_items[id] = name
+      end
+    end
+  end
+
+  erb :items, locals: {items: matched_items}
 end
